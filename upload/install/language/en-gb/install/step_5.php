b450ad23b41380fa3b60abfe605ebea6edab2f35
bernson-dev <bernson@tutamail.com>
2026-08-17 23:58:59 +0300

    Update installer completion localization

--- upload/install/language/en-gb/install/step_5.php
+++ upload/install/language/en-gb/install/step_5.php
@@ -3,19 +3,19 @@
 $_['heading_title'] = 'Installation complete';
 
 // Text
-$_['text_step_4'] = 'Ready to start selling!';
-$_['text_catalog'] = 'Go to your Online Shop';
-$_['text_admin'] = 'Login to your Administration';
-$_['text_extension'] = 'Visit the extensions store';
-$_['text_install_deleted'] = 'Delete Install folder after installation completes';
-
-$_['text_success_install_deleted']   = 'The install folder has been successfully deleted!';
+$_['text_step_5'] = 'You can now configure your store and start adding products!';
+$_['text_catalog'] = 'Go to your online shop';
+$_['text_admin'] = 'Go to administration';
+$_['text_extension'] = 'Visit the extensions marketplace';
+$_['text_install_deleted'] = 'Delete the install folder when leaving this page';
+$_['text_success_install_deleted'] = 'The install folder has been successfully deleted!';
 
 // Error
-$_['error_warning'] = 'Don\'t forget to delete your installation directory!';
-$_['text_error_install_delete']      = 'Failed to delete the install folder. Check permissions.';
-$_['text_error_install_not_found']   = 'Install folder not found.';
-$_['text_error_invalid_request']     = 'Invalid request type.';
-$_['text_error_dir_not_found']       = 'Directory not found.';
-$_['text_error_file_delete']         = 'Failed to delete a file inside the install folder.';
-$_['text_error_dir_delete']          = 'Failed to delete a directory inside the install folder.';
+$_['error_warning'] = 'Do not forget to delete the <b>install</b> directory from your site root!';
+$_['text_error_install_delete'] = 'Could not delete the install folder. Check permissions and try again.';
+$_['text_error_install_not_found'] = 'The install folder was not found.';
+$_['text_error_invalid_request'] = 'Invalid request type.';
+$_['text_error_invalid_session'] = 'The installation session is no longer valid. Reload the installation completion page.';
+$_['text_error_dir_not_found'] = 'The install directory was not found or has an invalid type.';
+$_['text_error_file_delete'] = 'Could not delete a file inside the install folder.';
+$_['text_error_dir_delete'] = 'Could not delete a directory inside the install folder.';
