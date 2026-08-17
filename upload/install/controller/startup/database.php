5fdb1c1bb428cbde313fa8c4206202186ce4e0a1
bernson-dev <bernson@tutamail.com>
2026-08-18 00:03:43 +0300

    Make installer database startup PHP 7.3 compatible

--- upload/install/controller/startup/database.php
+++ upload/install/controller/startup/database.php
@@ -1,22 +1,40 @@
 <?php
 class ControllerStartupDatabase extends Controller {
 	public function index() {
-		if (is_file(DIR_OPENCART . 'config.php') && filesize(DIR_OPENCART . 'config.php') > 0) {
-			$lines = file(DIR_OPENCART . 'config.php');
-			
-			foreach ($lines as $line) {
-				if (str_contains(strtoupper($line), 'DB_')) {
-					eval($line);
-				}
-			}
-			
-			if (defined('DB_PORT')) {
-				$port = DB_PORT;
-			} else {
-				$port = ini_get('mysqli.default_port');
-			}
-			
-			$this->registry->set('db', new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE, $port));
+		$config_file = DIR_OPENCART . 'config.php';
+
+		if (!is_file($config_file) || filesize($config_file) === 0) {
+			return;
 		}
+
+		require_once($config_file);
+
+		if (
+			!defined('DB_DRIVER') ||
+			!defined('DB_HOSTNAME') ||
+			!defined('DB_USERNAME') ||
+			!defined('DB_PASSWORD') ||
+			!defined('DB_DATABASE')
+		) {
+			return;
+		}
+
+		if (defined('DB_PORT') && DB_PORT !== '') {
+			$port = DB_PORT;
+		} else {
+			$port = ini_get('mysqli.default_port');
+		}
+
+		$this->registry->set(
+			'db',
+			new DB(
+				DB_DRIVER,
+				html_entity_decode(DB_HOSTNAME, ENT_QUOTES, 'UTF-8'),
+				html_entity_decode(DB_USERNAME, ENT_QUOTES, 'UTF-8'),
+				html_entity_decode(DB_PASSWORD, ENT_QUOTES, 'UTF-8'),
+				html_entity_decode(DB_DATABASE, ENT_QUOTES, 'UTF-8'),
+				$port
+			)
+		);
 	}
 }
