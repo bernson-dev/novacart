56ce10134063c517c7c583fcd3f0f14133a94a24
bernson-dev <bernson@tutamail.com>
2026-08-20 22:16:16 +0300

    Handle locked installer entry point on Windows

--- upload/install/controller/install/step_5.php
+++ upload/install/controller/install/step_5.php
@@ -31,9 +31,11 @@ class ControllerInstallStep5 extends Controller {
 
 				if ($result === true) {
 					$json['success'] = $this->language->get('text_success_install_deleted');
-				} elseif ($result === 'text_error_dir_delete' && $this->isDirectoryEmpty($install_path)) {
-					// На Windows/PHP 8.x текущий запрос может удерживать сам каталог install.
-					// Содержимое уже удалено, поэтому откладываем только финальный rmdir().
+				} elseif ($result === 'text_error_dir_delete' && $this->canDeferInstallRemoval($install_path)) {
+					// На Windows/PHP 8.x исполняемый install/index.php может оставаться
+					// заблокированным до завершения текущего HTTP-запроса.
+					// Всё остальное уже удалено, поэтому финальную очистку выполняет
+					// system/startup.php при следующем запросе к магазину или админке.
 					if (@file_put_contents(DIR_STORAGE . 'install_cleanup.flag', '1', LOCK_EX) !== false) {
 						$json['success'] = $this->language->get('text_success_install_deleted');
 					} else {
@@ -70,7 +72,7 @@ class ControllerInstallStep5 extends Controller {
 					return $result;
 				}
 			} else {
-				if (!unlink($path)) {
+				if (!@unlink($path) && file_exists($path)) {
 					return 'text_error_file_delete';
 				}
 			}
@@ -83,13 +85,20 @@ class ControllerInstallStep5 extends Controller {
 		return true;
 	}
 
-	private function isDirectoryEmpty($dir) {
+	private function canDeferInstallRemoval($dir) {
 		if (!is_dir($dir)) {
 			return false;
 		}
 
-		$files = scandir($dir);
+		clearstatcache(true, $dir);
+		$files = @scandir($dir);
 
-		return $files !== false && count($files) === 2;
+		if ($files === false) {
+			return false;
+		}
+
+		$files = array_values(array_diff($files, array('.', '..')));
+
+		return $files === array('index.php') && is_file($dir . DIRECTORY_SEPARATOR . 'index.php');
 	}
 }
