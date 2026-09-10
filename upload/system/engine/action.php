e3831bc7df8aecf2043af4399f7d9432ba1e4d51
bernson-dev <bernson@tutamail.com>
2026-09-11 00:48:28 +0300

    Validate controller method visibility in Action

--- upload/system/engine/action.php
+++ upload/system/engine/action.php
@@ -70,10 +70,14 @@ class Action {
 
 		$reflection = new ReflectionClass($class);
 
-		if ($reflection->hasMethod($this->method) && $reflection->getMethod($this->method)->getNumberOfRequiredParameters() <= count($args)) {
-			return call_user_func_array(array($controller, $this->method), $args);
-		} else {
-			return new \Exception('Error: Could not call ' . $this->route . '/' . $this->method . '!');
+		if ($reflection->hasMethod($this->method)) {
+			$method = $reflection->getMethod($this->method);
+
+			if ($method->isPublic() && $method->getNumberOfRequiredParameters() <= count($args)) {
+				return call_user_func_array(array($controller, $this->method), $args);
+			}
 		}
+
+		return new \Exception('Error: Could not call ' . $this->route . '/' . $this->method . '!');
 	}
 }
