897cc026c02ac9c8279caba38449e3ec5de802df
bernson-dev <bernson@tutamail.com>
2026-09-18 17:08:18 +0300

    Harden API shipping validation

--- upload/catalog/controller/api/shipping.php
+++ upload/catalog/controller/api/shipping.php
@@ -57,11 +57,22 @@ class ControllerApiShipping extends Controller {
 					$json['error']['postcode'] = $this->language->get('error_postcode');
 				}
 
-				if ($this->request->post['country_id'] == '') {
+				if (!$country_info) {
 					$json['error']['country'] = $this->language->get('error_country');
 				}
 
-				if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '') {
+				$this->load->model('localisation/zone');
+
+				$zones = $country_info ? $this->model_localisation_zone->getZonesByCountryId((int)$this->request->post['country_id']) : array();
+				$zone_ids = array();
+
+				foreach ($zones as $zone) {
+					$zone_ids[] = (int)$zone['zone_id'];
+				}
+
+				$zone_id = (int)$this->request->post['zone_id'];
+
+				if (($zone_ids && !in_array($zone_id, $zone_ids, true)) || (!$zone_ids && $zone_id !== 0)) {
 					$json['error']['zone'] = $this->language->get('error_zone');
 				}
 
@@ -72,9 +83,13 @@ class ControllerApiShipping extends Controller {
 
 				foreach ($custom_fields as $custom_field) {
 					if ($custom_field['location'] == 'address') {
-						if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']])) {
+						$custom_field_value = isset($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']])
+							? $this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']]
+							: '';
+
+						if ($custom_field['required'] && empty($custom_field_value)) {
 							$json['error']['custom_field' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
-						} elseif (($custom_field['type'] == 'text') && !empty($custom_field['validation']) && !filter_var($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']], FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => $custom_field['validation'])))) {
+						} elseif (($custom_field['type'] == 'text') && !empty($custom_field['validation']) && $custom_field_value !== '' && !filter_var($custom_field_value, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => $custom_field['validation'])))) {
 							$json['error']['custom_field' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
 						}
 					}
@@ -223,10 +238,10 @@ class ControllerApiShipping extends Controller {
 				// Shipping Method
 				if (empty($this->session->data['shipping_methods'])) {
 					$json['error'] = $this->language->get('error_no_shipping');
-				} elseif (!isset($this->request->post['shipping_method'])) {
+				} elseif (!isset($this->request->post['shipping_method']) || !is_scalar($this->request->post['shipping_method'])) {
 					$json['error'] = $this->language->get('error_method');
 				} else {
-					$shipping = explode('.', $this->request->post['shipping_method']);
+					$shipping = explode('.', (string)$this->request->post['shipping_method']);
 
 					if (!isset($shipping[0]) || !isset($shipping[1]) || !isset($this->session->data['shipping_methods'][$shipping[0]]['quote'][$shipping[1]])) {
 						$json['error'] = $this->language->get('error_method');
