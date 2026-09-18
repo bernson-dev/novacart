72ce07e319a1834404c6948b061808f455f2591a
bernson-dev <bernson@tutamail.com>
2026-09-18 16:36:38 +0300

    Harden checkout country endpoint

--- upload/catalog/controller/checkout/checkout.php
+++ upload/catalog/controller/checkout/checkout.php
@@ -99,7 +99,8 @@ class ControllerCheckoutCheckout extends Controller {
 
 		$this->load->model('localisation/country');
 
-		$country_info = $this->model_localisation_country->getCountry($this->request->get['country_id']);
+		$country_id = isset($this->request->get['country_id']) ? (int)$this->request->get['country_id'] : 0;
+		$country_info = $this->model_localisation_country->getCountry($country_id);
 
 		if ($country_info) {
 			$this->load->model('localisation/zone');
@@ -111,7 +112,7 @@ class ControllerCheckoutCheckout extends Controller {
 				'iso_code_3'        => $country_info['iso_code_3'],
 				'address_format'    => $country_info['address_format'],
 				'postcode_required' => $country_info['postcode_required'],
-				'zone'              => $this->model_localisation_zone->getZonesByCountryId($this->request->get['country_id']),
+				'zone'              => $this->model_localisation_zone->getZonesByCountryId($country_id),
 				'status'            => $country_info['status']
 			);
 		}
