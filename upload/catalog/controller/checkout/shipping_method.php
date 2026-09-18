634d784cccb9c3739b80ca732327002d15d74ed2
bernson-dev <bernson@tutamail.com>
2026-09-18 15:55:27 +0300

    Handle missing shipping comment safely

--- upload/catalog/controller/checkout/shipping_method.php
+++ upload/catalog/controller/checkout/shipping_method.php
@@ -121,7 +121,7 @@ class ControllerCheckoutShippingMethod extends Controller {
 		if (!$json) {
 			$this->session->data['shipping_method'] = $this->session->data['shipping_methods'][$shipping[0]]['quote'][$shipping[1]];
 
-			$this->session->data['comment'] = strip_tags($this->request->post['comment']);
+			$this->session->data['comment'] = isset($this->request->post['comment']) ? strip_tags($this->request->post['comment']) : '';
 		}
 
 		$this->response->addHeader('Content-Type: application/json');
