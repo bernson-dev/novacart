681dc0d7c3f9aab469c71605b969ce305c2299f1
bernson-dev <bernson@tutamail.com>
2026-09-18 11:58:53 +0300

    Remove unused download country localization

--- upload/admin/language/en-gb/catalog/download.php
+++ upload/admin/language/en-gb/catalog/download.php
@@ -15,7 +15,6 @@ $_['column_name']       = 'Download Name';
 $_['column_ip']         = 'IP';
 $_['column_account']    = 'Accounts';
 $_['column_store']      = 'Store';
-$_['column_country']    = 'Country';
 $_['column_date_added'] = 'Date Added';
 $_['column_action']     = 'Action';
 
