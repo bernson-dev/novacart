a3ee83f13fedf067a0c243c87605c22f096dee78
bernson-dev <bernson@tutamail.com>
2026-09-18 15:55:15 +0300

    Harden payment address validation

--- upload/catalog/controller/checkout/payment_address.php
+++ upload/catalog/controller/checkout/payment_address.php
@@ -105,6 +105,22 @@ class ControllerCheckoutPaymentAddress extends Controller {
 					unset($this->session->data['payment_methods']);
 				}
 			} else {
+				$defaults = array(
+					'firstname' => '',
+					'lastname' => '',
+					'address_1' => '',
+					'postcode' => '',
+					'city' => '',
+					'country_id' => 0,
+					'zone_id' => 0
+				);
+
+				foreach ($defaults as $key => $value) {
+					if (!isset($this->request->post[$key])) {
+						$this->request->post[$key] = $value;
+					}
+				}
+
 				if ((utf8_strlen(trim($this->request->post['firstname'])) < 1) || (utf8_strlen(trim($this->request->post['firstname'])) > 32)) {
 					$json['error']['firstname'] = $this->language->get('error_firstname');
 				}
@@ -129,11 +145,22 @@ class ControllerCheckoutPaymentAddress extends Controller {
 					$json['error']['postcode'] = $this->language->get('error_postcode');
 				}
 
-				if ($this->request->post['country_id'] == '') {
+				if (!$country_info) {
 					$json['error']['country'] = $this->language->get('error_country');
 				}
 
-				if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['zone_id'])) {
+				$this->load->model('localisation/zone');
+
+				$zones = $country_info ? $this->model_localisation_zone->getZonesByCountryId((int)$this->request->post['country_id']) : array();
+				$zone_ids = array();
+
+				foreach ($zones as $zone) {
+					$zone_ids[] = (int)$zone['zone_id'];
+				}
+
+				$zone_id = (int)$this->request->post['zone_id'];
+
+				if (($zone_ids && !in_array($zone_id, $zone_ids, true)) || (!$zone_ids && $zone_id !== 0)) {
 					$json['error']['zone'] = $this->language->get('error_zone');
 				}
 
@@ -144,9 +171,13 @@ class ControllerCheckoutPaymentAddress extends Controller {
 
 				foreach ($custom_fields as $custom_field) {
 					if ($custom_field['location'] == 'address') {
-						if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']])) {
+						$custom_field_value = isset($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']])
+							? $this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']]
+							: '';
+
+						if ($custom_field['required'] && empty($custom_field_value)) {
 							$json['error']['custom_field' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
-						} elseif (($custom_field['type'] == 'text') && !empty($custom_field['validation']) && !filter_var($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']], FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => $custom_field['validation'])))) {
+						} elseif (($custom_field['type'] == 'text') && !empty($custom_field['validation']) && $custom_field_value !== '' && !filter_var($custom_field_value, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => $custom_field['validation'])))) {
 							$json['error']['custom_field' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
 						}
 					}
