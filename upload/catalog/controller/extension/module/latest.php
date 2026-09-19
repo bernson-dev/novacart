def833cb829e3c2b5b4f8fed82768e4b1bfff32c
bernson-dev <bernson@tutamail.com>
2026-09-19 15:43:57 +0300

    Expose stock purchase policy in product lists

--- upload/catalog/controller/extension/module/latest.php
+++ upload/catalog/controller/extension/module/latest.php
@@ -9,6 +9,8 @@ class ControllerExtensionModuleLatest extends Controller {
 
 		$data['products'] = array();
 
+		$this->load->model('catalog/stock_policy');
+
 		$results = $this->model_catalog_product->getLatestProducts($setting['limit']);
 
 		if ($results) {
@@ -45,7 +47,9 @@ class ControllerExtensionModuleLatest extends Controller {
 					$rating = false;
 				}
 
-				$data['products'][] = array(
+								$stock_policy = $this->model_catalog_stock_policy->getListPolicy($result);
+
+$data['products'][] = array(
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
@@ -54,6 +58,10 @@ class ControllerExtensionModuleLatest extends Controller {
 					'special'     => $special,
 					'tax'         => $tax,
 					'rating'      => $rating,
+					'can_buy'     => $stock_policy['can_buy'],
+					'button_text' => $stock_policy['button_text'],
+					'stock_action'=> $stock_policy['action'],
+					'stock_button'=> (string)$this->config->get('config_stock_purchase_button'),
 					'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'])
 				);
 			}
