dc3f2c05f30661a68ea17d600f77b3564d942fc5
bernson-dev <bernson@tutamail.com>
2026-09-19 15:48:33 +0300

    Clarify remaining stock message

--- upload/catalog/language/en-gb/checkout/cart.php
+++ upload/catalog/language/en-gb/checkout/cart.php
@@ -41,7 +41,7 @@ $_['text_stock_popup_quantity'] = 'In cart';
 $_['text_stock_popup_available'] = 'Available';
 $_['button_stock_popup_close']  = 'OK';
 
-$_['error_stock_available']   = 'Insufficient stock. Available: %s.';
+$_['error_stock_available']   = 'Insufficient stock. You can add %s more.';
 $_['error_stock_unavailable'] = 'This product is currently unavailable for purchase.';
 $_['error_stock']              = 'Products marked with *** are not available in the desired quantity or not in stock!';
 $_['error_minimum']            = 'Minimum order amount for %s is %s!';
