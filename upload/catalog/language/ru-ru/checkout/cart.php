750b2947c47cdbaa229e98ab4f89a44484a993d8
bernson-dev <bernson@tutamail.com>
2026-09-19 17:56:54 +0300

    Localize preorder status in cart

--- upload/catalog/language/ru-ru/checkout/cart.php
+++ upload/catalog/language/ru-ru/checkout/cart.php
@@ -61,3 +61,6 @@ https://translator.codeguild.com.ua/
 $_['text_payment_cancel']                     = "%s каждые %d %s(s) до отмены";
 
 $_['text_stock_preorder_allowed'] = 'Товар доступен для заказа по правилу предзаказа, несмотря на текущий остаток.';
+
+$_['text_stock_preorder_badge'] = 'Предзаказ';
+$_['text_stock_preorder_cart'] = 'Товара сейчас нет в наличии, но заказ разрешён.';
