39e079a47e28f58f44b7dcb6b5e693dfa5d7699f
bernson-dev <bernson@tutamail.com>
2026-09-19 18:43:06 +0300

    Add in-cart button localization

--- upload/catalog/language/en-gb/en-gb.php
+++ upload/catalog/language/en-gb/en-gb.php
@@ -26,6 +26,7 @@ $_['button_address_add']    = 'Add Address';
 $_['button_back']           = 'Back';
 $_['button_continue']       = 'Continue';
 $_['button_cart']           = 'Add to Cart';
+$_['button_in_cart']        = 'In Cart';
 $_['button_cancel']         = 'Cancel';
 $_['button_compare']        = 'Compare this Product';
 $_['button_wishlist']       = 'Add to Wish List';
