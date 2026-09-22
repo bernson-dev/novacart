947c0f6d62378f46fb95b80caec777d24e759179
bernson-dev <bernson@tutamail.com>
2026-09-23 01:07:30 +0300

    fix: reject SEO keywords reserved by language prefixes

--- upload/admin/controller/catalog/information.php
+++ upload/admin/controller/catalog/information.php
@@ -682,6 +682,10 @@ class ControllerCatalogInformation extends Controller {
 					if (!empty($keyword)) {
 						$language_scoped = $this->model_design_seo_url->usesLanguageScopedKeywords((int)$store_id);
 
+						if ($this->model_design_seo_url->isReservedLanguagePrefix($keyword, (int)$store_id)) {
+							$this->error['keyword'][$store_id][$language_id] = $this->language->get('error_keyword');
+						}
+
 						if (!$language_scoped && count(array_keys($language, $keyword)) > 1) {
 							$this->error['keyword'][$store_id][$language_id] = $this->language->get('error_unique');
 						}
