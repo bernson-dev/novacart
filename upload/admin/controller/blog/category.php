cdb53810dd60d381415f338a3cb603649e6fb597
bernson-dev <bernson@tutamail.com>
2026-09-23 01:05:28 +0300

    feat: allow shared SEO slugs across languages

--- upload/admin/controller/blog/category.php
+++ upload/admin/controller/blog/category.php
@@ -540,11 +540,17 @@ class ControllerBlogCategory extends Controller {
 			foreach ($this->request->post['category_seo_url'] as $store_id => $language) {
 				foreach ($language as $language_id => $keyword) {
 					if (!empty($keyword)) {
-						if (count(array_keys($language, $keyword)) > 1) {
+						$language_scoped = $this->model_design_seo_url->usesLanguageScopedKeywords((int)$store_id);
+
+						if (!$language_scoped && count(array_keys($language, $keyword)) > 1) {
 							$this->error['keyword'][$store_id][$language_id] = $this->language->get('error_unique');
 						}
 
-						$seo_urls = $this->model_design_seo_url->getSeoUrlsByKeyword($keyword);
+						$seo_urls = $this->model_design_seo_url->getSeoUrlsByKeyword(
+							$keyword,
+							(int)$store_id,
+							$language_scoped ? (int)$language_id : null
+						);
 
 						foreach ($seo_urls as $seo_url) {
 							if (($seo_url['store_id'] == $store_id) && (!isset($this->request->get['blog_category_id']) || ($seo_url['query'] != 'blog_category_id=' . $this->request->get['blog_category_id']))) {
