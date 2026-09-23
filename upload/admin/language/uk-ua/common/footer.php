7cf51109a561bf24e8225efc66cc7242a06e969f
bernson-dev <bernson@tutamail.com>
2026-09-23 20:21:02 +0300

    i18n: simplify admin footer diagnostics

--- upload/admin/language/uk-ua/common/footer.php
+++ upload/admin/language/uk-ua/common/footer.php
@@ -1,6 +1,13 @@
 <?php
 
 // Text
-$_['text_footer']         = 'NovaCart &copy; 2009-' . date('Y') . ' Всі права захищені.';
-$_['text_version']        = 'Версія %s';
-$_['text_display_errors'] = 'Warning: PHP error display is enabled! Not recommended for production sites. <br>To disable, go to System → Settings → Server tab and uncheck "Show errors".';
+$_['text_footer']              = 'NovaCart &copy; 2009-' . date('Y') . ' Всі права захищені.';
+$_['text_version']             = 'Версія %s';
+$_['text_php']                 = 'PHP';
+$_['text_ioncube']             = 'IonCube';
+$_['text_not_installed']       = 'не встановлено';
+$_['text_https_secure']        = 'HTTPS';
+$_['text_https_insecure']      = 'HTTP';
+$_['text_server_time']         = 'Сервер';
+$_['text_display_errors']      = 'Увімкнено показ помилок PHP. Для робочого магазину це не рекомендується.';
+$_['text_display_errors_link'] = 'Вимкнути в налаштуваннях';
