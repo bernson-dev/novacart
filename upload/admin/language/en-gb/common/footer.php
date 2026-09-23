5f33787264742faf0f9806ddb36fb2cca42888ee
bernson-dev <bernson@tutamail.com>
2026-09-23 20:21:04 +0300

    i18n: simplify admin footer diagnostics

--- upload/admin/language/en-gb/common/footer.php
+++ upload/admin/language/en-gb/common/footer.php
@@ -1,6 +1,12 @@
 <?php
 // Text
-$_['text_footer']         = 'NovaCart &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-
-$_['text_version']        = 'Version %s';
-$_['text_display_errors'] = 'Warning: PHP error display is enabled! This is not recommended on a live site. <br> To disable it, go to System → Settings → "Server" tab and turn off "Display Errors".';
+$_['text_footer']              = 'NovaCart &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version']             = 'Version %s';
+$_['text_php']                 = 'PHP';
+$_['text_ioncube']             = 'IonCube';
+$_['text_not_installed']       = 'not installed';
+$_['text_https_secure']        = 'HTTPS';
+$_['text_https_insecure']      = 'HTTP';
+$_['text_server_time']         = 'Server';
+$_['text_display_errors']      = 'PHP error display is enabled. This is not recommended on a live store.';
+$_['text_display_errors_link'] = 'Disable in settings';
