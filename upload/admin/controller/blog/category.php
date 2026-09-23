059334b61c90937bb165c4bc306abb82180695ab
bernson-dev <bernson@tutamail.com>
2026-09-23 12:06:13 +0300

    feat: normalize entity SEO slugs across languages

--- upload/admin/controller/blog/category.php
+++ upload/admin/controller/blog/category.php
@@ -516,6 +516,14 @@ class ControllerBlogCategory extends Controller {
 	}
 
 	protected function validateForm() {
+		$this->load->model('design/seo_url');
+
+		if (isset($this->request->post['category_seo_url'])) {
+			$this->request->post['category_seo_url'] = $this->model_design_seo_url->normalizeLanguageScopedSeoUrls(
+				$this->request->post['category_seo_url']
+			);
+		}
+
 		if (!$this->user->hasPermission('modify', 'blog/category')) {
 			$this->error['warning'] = $this->language->get('error_permission');
 		}
@@ -535,8 +543,6 @@ class ControllerBlogCategory extends Controller {
 		}
 
 		if ($this->request->post['category_seo_url']) {
-			$this->load->model('design/seo_url');
-
 			foreach ($this->request->post['category_seo_url'] as $store_id => $language) {
 				foreach ($language as $language_id => $keyword) {
 					if (!empty($keyword)) {
