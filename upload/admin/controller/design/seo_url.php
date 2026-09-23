07651e3f5c11ed9117a43c73c43bbb26102fab66
bernson-dev <bernson@tutamail.com>
2026-09-23 12:58:58 +0300

    cleanup: remove synthetic preview template

--- upload/admin/controller/design/seo_url.php
+++ upload/admin/controller/design/seo_url.php
@@ -389,7 +389,6 @@ class ControllerDesignSeoUrl extends Controller {
 				'source_name'=> isset($source['name']) ? $source['name'] : '',
 				'source_edit'=> $source_edit,
 				'preview'    => $this->model_design_seo_url->getSeoUrlPreview($result),
-				'preview_template' => $this->model_design_seo_url->getSeoUrlPreviewTemplate($result),
 				'issues'     => isset($audit['issues'][(int)$result['seo_url_id']])
 					? $audit['issues'][(int)$result['seo_url_id']]
 					: array(
