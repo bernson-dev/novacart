6dcc0e840b744fadc3421a54acf425828c75a806
bernson-dev <bernson@tutamail.com>
2026-09-23 08:01:22 +0300

    fix: format SEO audit summary in controller

--- upload/admin/controller/design/seo_url.php
+++ upload/admin/controller/design/seo_url.php
@@ -282,6 +282,14 @@ class ControllerDesignSeoUrl extends Controller {
 
 		$audit = $this->model_design_seo_url->getSeoUrlAudit();
 		$data['audit_summary'] = $audit['summary'];
+		$data['audit_summary_text'] = sprintf(
+			$this->language->get('text_audit_summary'),
+			$audit['summary']['all_rows'],
+			$audit['summary']['issue_rows'],
+			$audit['summary']['keyword_groups'],
+			$audit['summary']['query_groups'],
+			$audit['summary']['prefix_rows']
+		);
 
 		$seo_url_total = $this->model_design_seo_url->getTotalSeoUrls($filter_data);
 
