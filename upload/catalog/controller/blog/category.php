9901893efce38d5bde59e99763d57c396484a14c
bernson-dev <bernson@tutamail.com>
2026-09-24 18:22:10 +0300

    Keep blog subcategory counts consistent with listings

--- upload/catalog/controller/blog/category.php
+++ upload/catalog/controller/blog/category.php
@@ -188,8 +188,7 @@ class ControllerBlogCategory extends Controller {
 
 			foreach ($results as $result) {
 				$filter_data = array(
-				'filter_blog_category_id' => $result['blog_category_id'],
-				'filter_sub_category'     => true
+				'filter_blog_category_id' => $result['blog_category_id']
 				);
 
 				$data['categories'][] = array(
