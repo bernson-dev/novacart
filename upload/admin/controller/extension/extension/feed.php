7d95e33d430d852349015f0f083f160233ccaf3d
bernson-dev <bernson@tutamail.com>
2026-09-24 11:02:05 +0300

    Keep feed status in sync after AJAX uninstall

--- upload/admin/controller/extension/extension/feed.php
+++ upload/admin/controller/extension/extension/feed.php
@@ -88,15 +88,16 @@ class ControllerExtensionExtensionFeed extends Controller {
 		if ($files) {
 			foreach ($files as $file) {
 				$extension = basename($file, '.php');
+				$installed = in_array($extension, $extensions, true);
 
 				$this->load->language('extension/feed/' . $extension, 'extension');
 
 				$data['extensions'][] = array(
 					'name'      => $this->language->get('extension')->get('heading_title'),
-					'status'    => $this->config->get('feed_' . $extension . '_status') ? '<span class="label label-success">' . $this->language->get('text_enabled') . '</span>' : '<span class="label label-danger">' . $this->language->get('text_disabled') . '</span>',
+					'status'    => ($installed && $this->config->get('feed_' . $extension . '_status')) ? '<span class="label label-success">' . $this->language->get('text_enabled') . '</span>' : '<span class="label label-danger">' . $this->language->get('text_disabled') . '</span>',
 					'install'   => $this->url->link('extension/extension/feed/install', 'user_token=' . $this->session->data['user_token'] . '&extension=' . $extension, true),
 					'uninstall' => $this->url->link('extension/extension/feed/uninstall', 'user_token=' . $this->session->data['user_token'] . '&extension=' . $extension, true),
-					'installed' => in_array($extension, $extensions),
+					'installed' => $installed,
 					'edit'      => $this->url->link('extension/feed/' . $extension, 'user_token=' . $this->session->data['user_token'], true)
 				);
 			}
