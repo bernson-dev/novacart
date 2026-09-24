472543949b61f0ac3d9e8d526bf6c1b0673e3759
bernson-dev <bernson@tutamail.com>
2026-09-24 14:59:04 +0300

    Add compare label to catalog header

--- upload/catalog/language/ru-ru/common/header.php
+++ upload/catalog/language/ru-ru/common/header.php
@@ -5,6 +5,7 @@
 // Text
 $_['text_home']          = 'Главная';
 $_['text_wishlist']      = 'Закладки (%s)';
+$_['text_compare']       = 'Сравнение товаров (%s)';
 $_['text_shopping_cart'] = 'Корзина';
 $_['text_category']      = 'Категории';
 $_['text_account']       = 'Личный кабинет';
