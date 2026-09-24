56fca6fa67cc2ca7c314ae88e395e8d755f2838d
bernson-dev <bernson@tutamail.com>
2026-09-24 11:03:31 +0300

    Keep theme status in sync after AJAX uninstall

--- upload/admin/controller/extension/extension/theme.php
+++ upload/admin/controller/extension/extension/theme.php
@@ -89,6 +89,7 @@ class ControllerExtensionExtensionTheme extends Controller {
 		if ($files) {
 			foreach ($files as $file) {
 				$extension = basename($file, '.php');
+				$installed = in_array($extension, $extensions, true);
 				
 				$this->load->language('extension/theme/' . $extension, 'extension');
 					
@@ -97,14 +98,14 @@ class ControllerExtensionExtensionTheme extends Controller {
 				$store_data[] = array(
 					'name'   => $this->config->get('config_name'),
 					'edit'   => $this->url->link('extension/theme/' . $extension, 'user_token=' . $this->session->data['user_token'] . '&store_id=0', true),
-					'status' => $this->config->get('theme_' . $extension . '_status') ? '<span class="label label-success">' . $this->language->get('text_enabled') . '</span>' : '<span class="label label-danger">' . $this->language->get('text_disabled') . '</span>'
+					'status' => ($installed && $this->config->get('theme_' . $extension . '_status')) ? '<span class="label label-success">' . $this->language->get('text_enabled') . '</span>' : '<span class="label label-danger">' . $this->language->get('text_disabled') . '</span>'
 				);
 
 				foreach ($stores as $store) {
 					$store_data[] = array(
 						'name'   => $store['name'],
 						'edit'   => $this->url->link('extension/theme/' . $extension, 'user_token=' . $this->session->data['user_token'] . '&store_id=' . $store['store_id'], true),
-						'status' => $this->model_setting_setting->getSettingValue('theme_' . $extension . '_status', $store['store_id']) ? '<span class="label label-success">' . $this->language->get('text_enabled') . '</span>' : '<span class="label label-danger">' . $this->language->get('text_disabled') . '</span>'
+						'status' => ($installed && $this->model_setting_setting->getSettingValue('theme_' . $extension . '_status', $store['store_id'])) ? '<span class="label label-success">' . $this->language->get('text_enabled') . '</span>' : '<span class="label label-danger">' . $this->language->get('text_disabled') . '</span>'
 					);
 				}
 				
@@ -112,7 +113,7 @@ class ControllerExtensionExtensionTheme extends Controller {
 					'name'      => $this->language->get('extension')->get('heading_title'),
 					'install'   => $this->url->link('extension/extension/theme/install', 'user_token=' . $this->session->data['user_token'] . '&extension=' . $extension, true),
 					'uninstall' => $this->url->link('extension/extension/theme/uninstall', 'user_token=' . $this->session->data['user_token'] . '&extension=' . $extension, true),
-					'installed' => in_array($extension, $extensions),
+					'installed' => $installed,
 					'store'     => $store_data
 				);
 			}
